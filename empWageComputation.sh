9c5dd85f53631b928207aae721071b278424d18e
Sunilv390 <sunilv390@gmail.com>
2020-02-14 12:47:12 +0530

    Added: Employee is Absent or Present using IFELSE

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,15 @@
-WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM
+#!/bin/bash -x
+
+#WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM
+
+#CONSTANTS
+isPresent=1
+
+#USERINPUT
+randomCheck=$((RANDOM%2))
+if [ $isPresent -eq $randomCheck ]
+then
+	echo "Employee is Present "
+else
+	echo "Employee is Absent "
+fi
