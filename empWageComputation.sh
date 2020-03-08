72986baea0114178d164f135f78921c4dd93d142
Sunilv390 <sunilv390@gmail.com>
2020-03-08 22:09:05 +0530

    Refactor : Indentations and Constants

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,9 +4,9 @@
 
 #CONSTANTS
 WAGE_PER_HR=20
-ISFULLTIME=1
-ISPARTTIME=2
-WORKINGDAYS=20
+IS_FULLTIME=1
+IS_PARTTIME=2
+WORKING_DAYS=20
 WORK_HR_IN_MONTH=100
 
 #VARIABLE
@@ -15,38 +15,37 @@ totalWorkingDays=0
 
 #FUNCTIONS TO CHECK WORKING HOURS
 function getWorkHours() {
-	case $1 in
-		$ISFULLTIME)
-			empHrs=8
-			;;
-		$ISPARTTIME)
-			empHrs=4
-			;;
-		*)
-			empHrs=0
-			;;
-	esac
-	echo $empHrs
+   case $1 in
+      $IS_FULLTIME)
+         empHrs=8
+         ;;
+      $IS_PARTTIME)
+         empHrs=4
+         ;;
+      *)
+         empHrs=0
+         ;;
+   esac
+   echo $empHrs
 }
 
 #FUNCTION FOR CALCULATING DAILY WAGE
 function calculateDailyWage() {
-local empHrs=$1
-wage=$(($empHrs*$WAGE_PER_HR))
-echo $wage
+   local empHrs=$1
+   wage=$(($empHrs*$WAGE_PER_HR))
+   echo $wage
 }
 
 #CALLING THE FUNCTION USING WHILE
-while [[ $totalWorkingHr -lt $WORK_HR_IN_MONTH &&
-         $totalWorkingDays -lt $WORKINGDAYS ]]
+while [[ $totalWorkingHr -lt $WORK_HR_IN_MONTH && $totalWorkingDays -lt $WORKING_DAYS ]]
 do
-	randomCheck=$((RANDOM%3))
-	((totalWorkingDays++))
-	empHrs=$( getWorkHours $randomCheck )
-	totalWorkingHr=$(($totalWorkingHr+$empHrs))
+   randomCheck=$((RANDOM%3))
+   ((totalWorkingDays++))
+   empHrs=$( getWorkHours $randomCheck )
+   totalWorkingHr=$(($totalWorkingHr+$empHrs))
 
-#STORING DAILYWAGE IN ARRAY
-	dailyWage[$totalWorkingDays]=$( calculateDailyWage $empHrs )
+   #STORING DAILYWAGE IN ARRAY
+   dailyWage[$totalWorkingDays]=$( calculateDailyWage $empHrs )
 done
 totalSalary=$( calculateDailyWage $totalWorkingHr )
 echo "Daily Wage is ${dailyWage[@]} "
